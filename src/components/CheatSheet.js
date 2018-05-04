dfdd2bcb5b0ce0e33e34ea12ceb9aac28e34f681
thaborak <thaborak@gmail.com>
2018-05-04 15:47:43 -0400

    ascii console

--- src/components/CheatSheet.js
+++ src/components/CheatSheet.js
@@ -12,7 +12,21 @@ import NFLTable from './NFLTable'
 export class CheatSheet extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPlayers());
-    console.log("%chttps://www.linkedin.com/in/tom-h-759579a5/", "color: blue; font-size: x-large");
+    console.log(`%c
+
+ /$$$$$$$$                            /$$   /$$           /$$                                    /$$      
+|__  $$__/                           | $$  | $$          | $$                                   | $$      
+   | $$  /$$$$$$  /$$$$$$/$$$$       | $$  | $$  /$$$$$$ | $$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$ | $$   /$$
+   | $$ /$$__  $$| $$_  $$_  $$      | $$$$$$$$ |____  $$| $$__  $$ /$$__  $$ /$$__  $$|____  $$| $$  /$$/
+   | $$| $$    $$| $$   $$   $$      | $$__  $$  /$$$$$$$| $$    $$| $$    $$| $$    _/ /$$$$$$$| $$$$$$/ 
+   | $$| $$  | $$| $$ | $$ | $$      | $$  | $$ /$$__  $$| $$  | $$| $$  | $$| $$      /$$__  $$| $$_  $$ 
+   | $$|  $$$$$$/| $$ | $$ | $$      | $$  | $$|  $$$$$$$| $$$$$$$/|  $$$$$$/| $$     |  $$$$$$$| $$    $$
+   |__/   _____/ |__/ |__/ |__/      |__/  |__/  ______/|_______/    _____/ |__/        ______/|__/    _/
+                                                                                                          
+                                                                                                          
+                                                                                                           `, "font-family:monospace");
+
+console.log("%chttps://www.linkedin.com/in/tom-h-759579a5", "color: blue; font-size: x-large");
   }
   render() {
     if (this.props.loading) {
